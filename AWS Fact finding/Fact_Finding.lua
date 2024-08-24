6177f6cb90b17e2f21e707b09a598780dc701590
collins-netAdmin <collinsbboy2018@gmail.com>
2024-08-24 22:48:28 +0000

    AWS Notes For Cloud Practitioner

---
+++ AWS Fact finding/Fact_Finding.lua
@@ -0,0 +1,65 @@
+Python
+Python fact finding exercise 
+1.Define a listand tuplein Python. Provide some examples.
+2.What is a namespacein Python?
+3.What is the difference between a local variable and a global variable?
+4.What is an IDE? Mention some common IDEs that could be used with Python.
+5.What are modulesin Python? Provide some examples.
+6.What is the difference between an arrayand a list?
+7.What are operators? Provide some examples.
+
+Databases
+Databases fact finding exercise 
+1.What is the difference between a relationaland a non-relational database?
+2.What are indexes?
+3.What are primary keysand secondary keys?
+4.What are inner joinsand outer joins?
+5.What is the difference between DROP TABLEand TRUNCATE TABLE?
+6.What are the different data types in SQL?
+7.Explain the WHEREand HAVINGclauses.
+
+AWS Cloud Foundations –One
+AWS Cloud Foundations –One fact finding exercise 
+1.Define what IaaS, PaaSand SaaSis.
+2.Provide 6 advantages of cloud computing.
+3.Define what an AWS region and an Availability Zone is.
+4.List all the AWS regions.
+5.What are the categories in which the AWS services are grouped?
+6.What is the difference between object storage and block storage?
+7.List two AWS compute services and explain them.
+8.List two AWS storage services and explain them.
+
+AWS Cloud Foundations –Two
+AWS Cloud Foundations –Two fact finding exercise 
+1.Explain the AWS Shared responsibility model.
+2.Explain an AWS Identity and Access Management (IAM) Role.
+3.Explain an AWS Identity and Access Management (IAM) Policy.
+4.Describe an Amazon Machine Image (AMI).
+5.List the different Amazon EC2 instance types with use cases for each type.
+6.Explain Amazon Virtual Private Cloud (VPC).
+7.Differentiate between a Publicand a Privatesubnet.
+
+AWS Well-Architected Framework
+AWS Well-Architected Framework fact finding exercise 
+1.What are the five pillars of the Well Architected Framework (WAF)?
+2.What are the 3 areas of operational excellence in the cloud?
+3.What are the design principles that strengthen system security?
+4.What are the design principles that increase reliability?
+5.What are the areas to focus on to achieve performance efficiency in the cloud?
+6.What are the different approaches to using AWS resources in a cost-effective manner?
+
+AWS CloudFormation
+AWS CloudFormation fact finding exercise 
+1.What is Configuration Orchestration?
+2.What is Configuration Management? List some commonly used tools for Configuration Management.
+3.What is Continuous Integration?
+4.What is Continuous Delivery?
+5.What is AWS CloudFormation? List 3 advantages of CloudFormation.
+6.What is JSONand YAML? List 3 differences between them.
+7.What is a stackin AWS CloudFormation?
+
+AWS Billing
+AWS Billing fact finding exercise 
+1.List the different types of AWS support plans.
+2.Explain the AWS Simple Monthly Calculator.
+3.List and explain the different Amazon EC2 pricing models.
